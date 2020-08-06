db1d0826b60df307021bc95db748162b214b0c85
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-06 09:07:03 -0700

    Add timestamp to logger() middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,13 @@
+const moment = require("moment");
+
 const logger = (req, res, next) => {
   const reqMethod = req.method;
   const reqURL = req.url;
+  const currentTime = moment().format();
 
-  console.log(`A ${reqMethod} request was made to the '${reqURL}' route`);
+  console.log(
+    `A ${reqMethod} request was made to the '${reqURL}' route at ${currentTime}`
+  );
 
   next();
 };
